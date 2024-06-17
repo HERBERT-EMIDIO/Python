19a93f308ef8a6afb795c3cb6b8b2dc85d4c27f4
Herbert-Emidio <herbertemidio2017@gmail.com>
2024-06-17 12:08:50 -0300

    aula02

---
+++ master_class_python/aula02.py
@@ -0,0 +1,35 @@
+# usando o prompt do python 
+# ctr + shift + ' para abrir o terminal
+# py no terminal para entrar no prompt de comando
+ 
+ 
+
+
+#input => obs: o input retorna uma str
+nome = input("Digite seu nome: ") 
+
+# Casting conversão do tipo de dados do recebimento do input()=str para int
+idade = int(input("Digite seu nome: ")) 
+ 
+# criando uma função de apredsentacao():
+# o nome e a idade = recebido do input fica como default 
+def apresentacao(nome=nome, idade=idade):
+    print(f"Meu nome é {nome} e tenho {idade} anos de idade.")
+    
+    
+
+#chamando a função
+apresentacao()
+
+
+# operadores
+""" 
+    ==
+    > ou >=
+    < ou <=
+    !=
+    
+"""
+     
+ 
+ 
