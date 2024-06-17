a51d8e172d67d7fc3978232890554e00096fa607
Herbert-Emidio <herbertemidio2017@gmail.com>
2024-06-17 10:39:44 -0300

    Adiciona arquivos na branch aula_master_class_python

---
+++ master_class_python/teste.py
@@ -0,0 +1,13 @@
+#Função print()
+
+print("Olá, Pessoal!")
+
+""" 
+ e digita python nome_arquivo.py
+ botão play(run code) ou no terminal entra na pasta do arquivo "codig" 
+ 
+ obs: extensão: .run marcar a opcao Code-runner: run in terminal
+ para rodar direto no terminal em vez de rodar na saída 
+ 
+ resolver(Ol�, Pessoal!) essa 
+"""
