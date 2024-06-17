5202acc6db71f70ec908d30415d35cb6dc441b0d
Herbert-Emidio <herbertemidio2017@gmail.com>
2024-06-17 16:57:24 -0300

    duas pastas desafio e sistema_bancario

---
+++ Dio/desafios_dio/desafio_dio_AventuraExplorador.py
@@ -0,0 +1,8 @@
+# Desafio: A Aventura do Explorador
+
+# Entrada
+quantidade_passos = int(input())
+
+# //TODO: Adicione uma condição para verificar se a quantidade de passos é positiva.
+# // Se a quantidade de passos for zero, imprima "Nenhum passo dado na floresta."
+# // Caso contrário, utilize um loop for para imprimir a mensagem do explorador, indicando o número do passo.
