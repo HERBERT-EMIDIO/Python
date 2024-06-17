6e0a8cffc2c6db5da885aff4e2c387e6b3875c4f
Herbert-Emidio <herbertemidio2017@gmail.com>
2024-06-17 16:59:15 -0300

    masterClass_Visualizar_pastas

--- Dio/desafios_dio/desafio_dio_AventuraExplorador.py
+++ Dio/desafios_dio/desafio_dio_AventuraExplorador.py
@@ -1,8 +1,15 @@
 # Desafio: A Aventura do Explorador
 
-# Entrada
+# Entrada de um número inteiro positivo representando a quantidade de passos
 quantidade_passos = int(input())
 
-# //TODO: Adicione uma condição para verificar se a quantidade de passos é positiva.
-# // Se a quantidade de passos for zero, imprima "Nenhum passo dado na floresta."
-# // Caso contrário, utilize um loop for para imprimir a mensagem do explorador, indicando o número do passo.
+# TODO: Adicione uma condição para verificar se a quantidade de passos é positiva.
+# O programa deve imprimir uma mensagem indicando o progresso do explorador na floresta.
+# Utilize um laço de repetição para simular os passos do explorador.
+
+for i in quantidade_passos:
+    print(f"Explorador: {i+1} passo")
+    i+=1
+
+#  Se a quantidade de passos for zero, imprima "Nenhum passo dado na floresta."
+#  Caso contrário, utilize um loop for para imprimir a mensagem do explorador, indicando o número do passo.
